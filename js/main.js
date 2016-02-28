2ad1ef2a2b1cf5c5e11ee8637b4f6ae3dcd8027a
TryBr <avulsed.jb@gmail.com>
2016-02-29 01:17:00 +0500

    Сделан каталог, слайдер фильтры

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      singleItem : true,
+      responsive : true,
+      navigationText : false,
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+})
